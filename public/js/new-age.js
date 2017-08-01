0abb5fc6487f9bbebaeb8f1d269eab8e6cd04035
Anmol Parande <parandea1.7@gmail.com>
2017-08-01 18:02:34 -0500

    Fixed issue with callback not being called

--- public/js/new-age.js
+++ public/js/new-age.js
@@ -50,7 +50,8 @@
     }).on('success.form.bv', function(e) {
         e.preventDefault()
         var data = $("[name='betaEmail']").val()
-        $.post("http://ec2-13-59-198-207.us-east-2.compute.amazonaws.com:8000/sendEmail", {email:data, subject:"Beta Signup"}, function(result) {
+        $.post("http://localhost:8000/sendEmail", {email:data, subject:"Beta Signup"}, function(result) {
+            console.log(result);
             window.location.assign('thank-you.html');
         });
     });
@@ -94,7 +95,7 @@
         var email = $("[name='email']").val()
         var name = $("[name='Name']").val()
         var message = $("[name='Message']").val()
-        $.post("http://172.31.30.25:8000/sendEmail", {email:email, subject:"Contact", name: name, message: message}, function(result) {
+        $.post("http://localhost:8000/sendEmail", {email:email, subject:"Contact", name: name, message: message}, function(result) {
         });
 
         window.alert("Thank you for contacting us. We will get back to you soon");
